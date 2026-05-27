8683b40ea0c22bb68b08c3e3e965bd17131cec25
Nicole Quenamá <110855023+nicoleQuenama@users.noreply.github.com>
2026-05-27 14:29:13 -0400

    cambios nuevos en el main_labview

--- main_labview.py
+++ main_labview.py
@@ -1,33 +1,49 @@
 import sys
 import os
 
+#leemos la carpeta
 current_dir = os.path.dirname(os.path.abspath(__file__))
 if current_dir not in sys.path:
     sys.path.append(current_dir)
 
 from controlador import SimuladorPasteurizador
 
-def ejecutar_simulacion(metodo, tiempo_segundos):
+#variable global para el labview
+sim = None
+
+def iniciar_simulacion(metodo):
+    global sim
     sim = SimuladorPasteurizador()
     sim.iniciar(metodo)
 
-    tiempos = []
-    temperaturas = []
-    potencias = []
-    bacterias=[]
-    pasos = int(tiempo_segundos)
-    for _ in range(pasos):
-        sim.avanzar(dt=1.0)
+    return True 
 
-        tiempos.append(float(sim.tiempo /60.0))
-        temperaturas.append(float(sim.temp))
-        potencias.append(float(sim.potencia))
+def simulacion(kp, ki, kd, setpoint, ruido, inyectar_perturbacion):
+    global sim
 
-        #bacterias destruidas
-        if sim.n < sim.n_base:
-            pct = (1.0- sim.n/ sim.n_base)* 100.0
-        else:
-            pct = 0.0
-        bacterias.append(float(pct))
+    if sim is None:
+        return 0.0, 0.0, 0.0,0.0
+    
+    #valores para labview
+    sim.kp = kp
+    sim.ki = ki
+    sim.kd =kd
+    sim.temp_override = setpoint
+    sim.ruido_nivel = ruido
 
-    return tiempos,bacterias,temperaturas,potencias
+    #boton de perturbacion
+    if inyectar_perturbacion:
+        sim.perturbar(-10.0)
+
+    #avance del tiempo
+    sim.avanzar(dt=1.0)
+
+    #calculo de las bacterias
+    if sim.n < sim.n_base:
+        pct_bac = (1.0 - sim.n / sim.n_base) * 100.0
+    else:
+        pct_bac = 0.0
+
+    tiempo_min = float(sim.tiempo /60.0)
+
+    return tiempo_min, float(pct_bac), float(sim.temp), float(sim. potencia)
